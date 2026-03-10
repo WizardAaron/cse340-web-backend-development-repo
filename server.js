def74ca8405f88313a3ddafe19140f73f8384541
WizardAaron <ethanlong547@gmail.com>
2026-03-09 18:03:21 -0700

    added layout and partials for header, navigation, head, and footer

--- server.js
+++ server.js
@@ -8,7 +8,17 @@
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
+const expressLayouts = require("express-ejs-layouts")
 const static = require("./routes/static")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLaouts)
+app.set("layout", "./layouts/layout") // not at the views root
+
 
 /* ***********************
  * Routes

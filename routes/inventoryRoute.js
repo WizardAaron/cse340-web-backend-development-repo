14afaafdb7382748f4e35995934cad119a5d9e29
WizardAaron <ethanlong547@gmail.com>
2026-03-24 18:08:52 -0700

    Fixed invController file path

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,7 +1,7 @@
 // Needed Resources
 const express = require("express")
 const router = new express.Router()
-const invController = require("../controller/invController")
+const invController = require("../controllers/invController")
 
 // Route ro build inventory by classification view
 router.get("/type/:classificationID", invController.buildByClassificationId);

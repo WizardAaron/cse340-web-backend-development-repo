58d225f1c8d0c38a467de7ad5fd830c923efcc36
WizardAaron <ethanlong547@gmail.com>
2026-04-02 14:51:34 -0700

    Added server-side registration validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
 
 /* ***************************
  *  Account Routes
@@ -12,6 +13,11 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Route to build registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 // Process the registration data
-router.post("/register", utilities.handleErrors(accountController.registerAccount))
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router;
